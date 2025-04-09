08ea6ae15a451a74a4018ef314e2637d6d0dca96
Wallas Maciel <wallasmaciel919@gmail.com>
2025-04-08 22:27:48 -0300

    (feat) Funcao de 'countdown'.

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 const handlePassageTime = () => {
-  const time = new Date()
+  const now = new Date()
+  const dateEnd = new Date(now.getFullYear(), now.getMonth(), 19, 19, 0, 0)
+  //
+  let dateDiff = dateEnd - now
+  if (dateDiff <= 0) {
+    dateEnd.setMonth(dateEnd.getMonth() + 1)
+    dateDiff = dateEnd - now
+  }
+  //
+	const seconds = Math.floor((dateDiff / 1000) % 60)
+  const minutes = Math.floor((dateDiff / 1000 / 60) % 60)
+  const hours = Math.floor((dateDiff / (1000 * 60 * 60)) % 24)
+  const days = Math.floor(dateDiff / (1000 * 60 * 60 * 24))
   // 
   const spans = document.querySelectorAll(".card > span")
-  changeCountdown(spans[0], `${time.getDate()}`.padStart(2, "0"))
-  changeCountdown(spans[1], `${time.getHours()}`.padStart(2, "0"))
-  changeCountdown(spans[2], `${time.getMinutes()}`.padStart(2, "0"))
-  changeCountdown(spans[3], `${time.getSeconds()}`.padStart(2, "0"))
+  changeCountdown(spans[0], `${days}`.padStart(2, "0"))
+  changeCountdown(spans[1], `${hours}`.padStart(2, "0"))
+  changeCountdown(spans[2], `${minutes}`.padStart(2, "0"))
+  changeCountdown(spans[3], `${seconds}`.padStart(2, "0"))
 }
 const intervalPassageTime = setInterval(handlePassageTime, 1000)
 
